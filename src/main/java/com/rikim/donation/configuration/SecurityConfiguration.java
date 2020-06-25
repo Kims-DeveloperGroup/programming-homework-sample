7109f88008c7ba6064bef2b9cdba59c139277152
rikim <rikim@riotgames.com>
2020-06-25 19:51:33 +0900

    Add SpringSecurity configuration to perAll path and to disable csrf

---
+++ src/main/java/com/rikim/donation/configuration/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.rikim.donation.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("*").permitAll()
+                .anyRequest().permitAll();
+    }
+}
