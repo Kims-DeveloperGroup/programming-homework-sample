c52c8656c29a3677807db9ddfe534745b8a52d87
rikim <rikim@riotgames.com>
2020-06-26 12:55:12 +0900

    Add 403 status to PermissionNotAllowedAccess

--- src/main/java/com/rikim/donation/exception/PermissionNotAllowedAccess.java
+++ src/main/java/com/rikim/donation/exception/PermissionNotAllowedAccess.java
@@ -1,5 +1,9 @@
 package com.rikim.donation.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class PermissionNotAllowedAccess extends Exception {
     public PermissionNotAllowedAccess(String message) {
         super(message);
